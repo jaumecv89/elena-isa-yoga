eca1bc409025ccc087284cc3caf80ec4f2736d80
Jaume Campderros <jaume.cvila@gmail.com>
2023-08-14 12:52:05 +0200

    added intersection observer mock for testing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,3 +2,12 @@ import matchers from "@testing-library/jest-dom/matchers"
 import { expect } from "vitest"
 
 expect.extend(matchers)
+
+const IntersectionObserverMock = vi.fn(() => ({
+    disconnect: vi.fn(),
+    observe: vi.fn(),
+    takeRecords: vi.fn(),
+    unobserve: vi.fn(),
+}))
+
+vi.stubGlobal("IntersectionObserver", IntersectionObserverMock)
